4528d0dfb0cb4da26cf3e014a75d8762d415564d
neutron-pc <sandormatyas17@gmail.com>
2022-08-04 15:54:38 +0200

    max_brightness

--- bright.sh
+++ bright.sh
@@ -5,7 +5,19 @@ if [ "$#" -ne 1 ]; then
 	exit 1
 fi
 
-# if [ read -lt "$1" ]
+dir=(/sys/class/backlight/*)
+
+birghtness="$dir/brightness"
+max_brightness="255"
+
+while read line; do
+	max_brightness="$line"
+done < "$dir/max_brightness"
+
+if [ $max_brightness -lt "$1" ]; then
+	echo "Too high number, max brightness is $max_brightness"
+	exit 2
+fi
 
 echo "$1" | sudo tee /sys/class/backlight/amdgpu_bl1/brightness
 
